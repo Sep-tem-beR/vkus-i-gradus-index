4add7eaafb1ee0f9f2af3680e00363590d40ccf8
Евгений Пушкарёв <rosrys2013@mail.ru>
2020-05-24 23:56:35 +0300

    body parallax and .moonshine-card, .recipes-card

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import './index.scss'
 
 
 (function () {
-    const burger = document.querySelector('.moonshine-card__burger');
+    const burger = document.querySelector('.moonshine-card__button');
     const burgerMenu = document.querySelector('.moonshine-menu');
     burger.addEventListener('click', () => {
         burgerMenu.classList.add('moonshine-menu_active');
@@ -18,7 +18,7 @@ import './index.scss'
 }());
 
 (function () {
-    const burger = document.querySelector('.recipes-card__burger');
+    const burger = document.querySelector('.recipes-card__button');
     const burgerMenu = document.querySelector('.recipes-menu');
     burger.addEventListener('click', () => {
         burgerMenu.classList.add('recipes-menu_active');
@@ -37,7 +37,7 @@ import './index.scss'
 
 function header_parallax(event) {
     let a = document.querySelector('.header__background');
-    a.style.transform = 'translateX(' + event.clientX/100 + 'px)' + " " + 'translateY(' + event.clientY/100 + 'px)';
+    a.style.transform = 'translateX(' + event.clientX/150 + 'px)' + " " + 'translateY(' + event.clientY/150 + 'px)';
 };
 
 document.querySelector('.header').addEventListener('mousemove', header_parallax);
@@ -46,7 +46,20 @@ document.querySelector('.header').addEventListener('mousemove', header_parallax)
 
 function header_title_parallax(event) {
     let a = document.querySelector('.header__title.title');
-    a.style.transform = 'translateX(' + event.clientX/-50 + 'px)' + " " + 'translateY(' + event.clientY/-50 + 'px)';
+    a.style.transform = 'translateX(' + event.clientX/-150 + 'px)' + " " + 'translateY(' + event.clientY/-150 + 'px)';
 };
 
 document.querySelector('.header').addEventListener('mousemove', header_title_parallax);
+
+
+//---------------------------body_parallax---------------------------//
+
+window.addEventListener('scroll', function() {
+    let a = document.querySelector('.moonshine-card__title');
+    a.style.textShadow = pageYOffset/100 + 'px' + ' ' + pageYOffset/100 + 'px' + ' ' + pageYOffset/200 + 'px' +' '  + 'hsl(0, 0%, 50%)';
+});
+
+window.addEventListener('scroll', function() {
+    let a = document.querySelector('.recipes-card__title');
+    a.style.textShadow = pageYOffset/100 + 'px' + ' ' + pageYOffset/100 + 'px' + ' ' + pageYOffset/200 + 'px' +' '  + 'hsl(0, 0%, 50%)';
+});
